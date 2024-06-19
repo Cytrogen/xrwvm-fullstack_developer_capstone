4fa04837530afcd208fc86e1485880841c3cfcde
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-06-19 15:26:54 -0400

    PyLint Update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -87,9 +87,10 @@ def registration(request):
         return JsonResponse(data)
 
 
-def get_cars():
+def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
+    print(request)
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
@@ -104,17 +105,19 @@ def get_cars():
 
 # Update the `get_dealerships` render list of dealerships all by default,
 # particular state if state is passed
-def get_dealerships(state="All"):
+def get_dealerships(request, state="All"):
     if state == "All":
         endpoint = "/fetchDealers"
     else:
+        state = request.GET.get('state', 'All')
         endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(dealer_id):
+def get_dealer_reviews(request, dealer_id):
+    print(request)
     # if dealer id has been provided
     if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
@@ -129,7 +132,8 @@ def get_dealer_reviews(dealer_id):
 
 
 # Create a `get_dealer_details` view to render the dealer details
-def get_dealer_details(dealer_id):
+def get_dealer_details(request, dealer_id):
+    print(request)
     if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
